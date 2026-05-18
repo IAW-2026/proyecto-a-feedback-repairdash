0c0eea34a6836b5f0604c6534a8c553e71e5bcd7
FranRK <franciscorecaldee@gmail.com>
2026-05-18 19:12:12 -0300

    cambio inicializacion de prisma

--- feedbackapp/lib/prisma.ts
+++ feedbackapp/lib/prisma.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-// Dejamos la variable declarada pero vacía
-let prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-// Creamos una función que solo instancia a Prisma cuando se la llama
 export const getPrisma = () => {
-  if (!prisma) {
-    prisma = new PrismaClient();
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
   }
-  return prisma;
+  return globalForPrisma.prisma;
 };
