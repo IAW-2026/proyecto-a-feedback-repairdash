782c3cbad4d329e8131cf244d17048cf8af43f8f
FranRK <franciscorecaldee@gmail.com>
2026-06-09 18:19:09 -0300

    agregando validaciones en apis

--- feedbackapp/app/api/reviews/user/route.ts
+++ feedbackapp/app/api/reviews/user/route.ts
@@ -5,7 +5,7 @@ Request: { "idTrabajo": "trabajo-001" }
 Response 201 Created: { "message": "Reviews creadas y trabajo finalizado", "reviews": [{ "idUsuario": "user_123" }, { "idUsuario": "user_456" }] }*/
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
-
+import { validateAnyInternalApiKey } from "@/lib/auth";
 export const dynamic = 'force-dynamic';
 
 function validarStringID(value: unknown): value is string {
@@ -18,6 +18,12 @@ function nombreCompleto(usuario: { nombre: string; apellido: string }) {
 
 export async function POST(request: Request) {
   //Si no se envía un JSON. Catch() vuelve nulo a body
+  const authError = validateAnyInternalApiKey(request, [
+      process.env.FEEDBACK_API_KEY,
+      process.env.DRIVER_INTERNAL_API_KEY,
+    ]);
+    if (authError) return authError;
+    
   const body = await request.json().catch(() => null);
 
   if (!body) {
